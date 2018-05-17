03fe5214f492121017eee5a10c5901e4a1b564aa
Andrey Hernandez <hec_boy@hotmail.com>
2018-05-16 22:00:02 -0500

    Creacion de Tablas

--- League planner/App.xaml.cs
+++ League planner/App.xaml.cs
@@ -51,17 +51,38 @@ namespace League_planner
 
             // Creacion de tabla arbitros
             command.CommandText = "CREATE TABLE arbitros(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " nombre varchar(50), apellidop varchar(50), apellidom varchar(50), nacimiento DATE, telefono varchar(10));";
+                " nombre varchar(50), apellidop varchar(50), apellidom varchar(50), nacimiento DATE, telefono varchar(10), email varchar(100), eliminado int);";
             command.ExecuteNonQuery();
 
             // Creacion de tabla equipos
             command.CommandText = "CREATE TABLE equipos(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " nombre varchar(50), apellidop varchar(50), apellidom varchar(50));";
+                " nombre varchar(50), clave_entrenador int, eliminado int, activo int);";
             command.ExecuteNonQuery();
 
             // Creacion de tabla entrenadores
             command.CommandText = "CREATE TABLE entrenadores(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " nombre varchar(50), apellidop varchar(50), apellidom varchar(50));";
+                " nombre varchar(50), apellidop varchar(50), apellidom varchar(50), nacimiento DATE, telefono varchar(10), email varchar(100), eliminado int);";
+            command.ExecuteNonQuery();
+
+            // Creacion de tabla partidos
+            command.CommandText = "CREATE TABLE partidos(id INTEGER PRIMARY KEY AUTOINCREMENT," + 
+                                   "clave_equipo_v int, clave_equipo_l int, tipo_partido varchar(10), clave_arbitro int, clave_temporada int, fecha DATE);";
+            command.ExecuteNonQuery();
+
+            //Creacion de tabla temporadas
+            command.CommandText = "CREATE TABLE temporadas(id INTEGER PRIMARY KEY AUTOINCREMENT," + "periodo varchar(30), clave_campeon int );";
+            command.ExecuteNonQuery();
+
+            //Creacion de tabla goles
+            command.CommandText = "CREATE TABLE goles(id INTEGER PRIMARY KEY AUTOINCREMENT," + " clave_jugador int, clave_partido int, favor_o_contra int);";
+            command.ExecuteNonQuery();
+
+            //Creacion de tabla tarjetas
+            command.CommandText = "CREATE TABLE tarjetas(id INTEGER PRIMARY KEY AUTOINCREMENT," + "clave_jugador int, tipo varchar(10), clave_partido int);";
+            command.ExecuteNonQuery();
+
+            //Creacion de tabla usuarios
+            command.CommandText = "CREATE TABLE usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT," + "tipo varchar(30), contrasena varchar(20), eliminado int);";
             command.ExecuteNonQuery();
 
             //command.CommandText = "INSERT INTO jugadores(nombre, apellidop, apellidom) VALUES('Juan', 'Perez', 'Lopez'); ";
